1d76902daeada4bc16fe856df47ad6b9add05af2
gloriiaq <gloriaqian2020@gmail.com>
2021-03-27 20:45:00 -0400

    inital setup

---
+++ portfolio/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+#add blog app into setting, url, views, html ( might have done it already)
+
+# create your models here
+
+#title
+#pug_date DateTimeField
+#body TextField
+#image ImageField
+
+#makemigrations
+#migrate
+#add to admin
+#def __str__
+
+#update views
+#update allblog.html
